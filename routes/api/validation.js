165b1aba41745dfb0a1dc8b0a9f9bb5b5e0cdf28
Ruslan Tendora <ruslantendora@gmail.com>
2023-02-19 20:14:57 +0200

    add api functions

---
+++ routes/api/validation.js
@@ -0,0 +1,23 @@
+const Joi = require("joi");
+
+const postSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+})
+  .required()
+  .max(3);
+
+const putSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string().email(),
+  phone: Joi.string(),
+})
+  .required()
+  .min(1)
+  .max(3);
+
+module.exports = {
+  postSchema,
+  putSchema,
+};
